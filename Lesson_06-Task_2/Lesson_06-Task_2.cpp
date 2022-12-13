2ba3b03f2f1fa3f77f34404e1cc43605253d62d7
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-12-13 16:41:37 +0300

    a

---
+++ Lesson_06-Task_2/Lesson_06-Task_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const* argv[])
+{
+    srand(time(NULL));
+
+    for (size_t i = 0; i <= 100; i++)
+    {
+        int first = rand() % 7;
+        int second = rand() % 7;
+        printf("Summa ohckov: %d\n", first + second);
+    }
+
+    return 0;
+}
