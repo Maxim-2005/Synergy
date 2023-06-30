2ec300c44bd25383dff84cbcc427dd7565e44618
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2023-06-30 11:45:57 +0300

    replace

---
+++ Upgrade_09_Shop/SelectDB.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+void fillShaft(List<KeyValuePair<String^, int>>^ shaft) {
+    shaft->Add(KeyValuePair<String^, int>(L"Àêàöèÿ", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"Àíãëèéñêèé äóá", 6));
+    shaft->Add(KeyValuePair<String^, int>(L"Áîÿðûøíèê", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Áóçèíà", 10));
+    shaft->Add(KeyValuePair<String^, int>(L"Áóê", 5));
+    shaft->Add(KeyValuePair<String^, int>(L"Âèíîãðàä", 2));
+    shaft->Add(KeyValuePair<String^, int>(L"Âèøíåâîå äåðåâî", 5));
+    shaft->Add(KeyValuePair<String^, int>(L"Âÿç", 2));
+    shaft->Add(KeyValuePair<String^, int>(L"Ãðàá", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Ãðåöêèé îðåõ", 6));
+    shaft->Add(KeyValuePair<String^, int>(L"Ãðóøåâîå äåðåâî", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Åëü", 5));
+    shaft->Add(KeyValuePair<String^, int>(L"Èâà", 2));
+    shaft->Add(KeyValuePair<String^, int>(L"Êàøòàí", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Êåäð", 8));
+    shaft->Add(KeyValuePair<String^, int>(L"Êèçèë", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"Êèïàðèñ", 5));
+    shaft->Add(KeyValuePair<String^, int>(L"Êë¸í", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Êðàñíûé äóá", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"Ëàâðîâîå äåðåâî", 5));
+    shaft->Add(KeyValuePair<String^, int>(L"Ëèïà", 7));
+    shaft->Add(KeyValuePair<String^, int>(L"Ëèñòâåííèöà", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"Îëüõà", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Îðåøíèê", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"Îñèíà", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"Îñòðîëèñò", 1));
+    shaft->Add(KeyValuePair<String^, int>(L"Ïèõòà", 2));
+    shaft->Add(KeyValuePair<String^, int>(L"Ðîçîâîå äåðåâî", 6));
+    shaft->Add(KeyValuePair<String^, int>(L"Ðÿáèíà", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Ñåêâîéÿ", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"Ñîñíà", 2));
+    shaft->Add(KeyValuePair<String^, int>(L"Òåðíîâíèê", 5));
+    shaft->Add(KeyValuePair<String^, int>(L"Òèñ", 3));
+    shaft->Add(KeyValuePair<String^, int>(L"Òîïîëü", 4));
+    shaft->Add(KeyValuePair<String^, int>(L"×¸ðíûé îðåøíèê", 6));
+    shaft->Add(KeyValuePair<String^, int>(L"Ýáåíîâîå äåðåâî", 6));
+    shaft->Add(KeyValuePair<String^, int>(L"ßâîð (áåëûé êë¸í)", 11));
+    shaft->Add(KeyValuePair<String^, int>(L"ßáëîíÿ", 2));
+    shaft->Add(KeyValuePair<String^, int>(L"ßñåíü", 4));
+}
+
+void fillCore(List<KeyValuePair<String^, int>>^ core) {
+    core->Add(KeyValuePair<String^, int>(L"Âîëîñ åäèíîðîãà", 5));
+    core->Add(KeyValuePair<String^, int>(L"Ïåðî ôåíèêñà", 6));
+    core->Add(KeyValuePair<String^, int>(L"Ñåðäå÷íàÿ æèëà äðàêîíà", 3));
+    core->Add(KeyValuePair<String^, int>(L"×åøóÿ äðàêîíà", 7));
+    core->Add(KeyValuePair<String^, int>(L"Êîãîòü äðàêîíà", 7));
+    core->Add(KeyValuePair<String^, int>(L"Ïåðî ãèïïîãðèôà", 4));
+    core->Add(KeyValuePair<String^, int>(L"Âîëîñ âåéëû", 5));
+    core->Add(KeyValuePair<String^, int>(L"Âîëîñ ñôèíêñà", 3));
+    core->Add(KeyValuePair<String^, int>(L"Ïåðî ïåãàñà", 4));
+    core->Add(KeyValuePair<String^, int>(L"Øåðñòü ïåãàñà", 2));
+    core->Add(KeyValuePair<String^, int>(L"Ñóøåíûé êîðåíü ìàíäðàãîðû", 3));
+    core->Add(KeyValuePair<String^, int>(L"Çóá âàñèëèñêà", 8));
+    core->Add(KeyValuePair<String^, int>(L"Âîë÷üÿ øåðñòü", 3));
+    core->Add(KeyValuePair<String^, int>(L"×åøóÿ çìåè", 2));
+    core->Add(KeyValuePair<String^, int>(L"Æèëû èç êðûëà ëåòó÷åé ìûøè", 1));
+    core->Add(KeyValuePair<String^, int>(L"Êîãîòü ãðèôîíà", 2));
+    core->Add(KeyValuePair<String^, int>(L"×åøóÿ ñàëàìàíäðû", 3));
+    core->Add(KeyValuePair<String^, int>(L"Ïåðî îðëà", 4));
+    core->Add(KeyValuePair<String^, int>(L"Ïåðî âîðîíà", 2));
+    core->Add(KeyValuePair<String^, int>(L"Ñîâèíîå ïåðî", 3));
+    core->Add(KeyValuePair<String^, int>(L"Ñîêîëèíîå ïåðî", 2));
+    core->Add(KeyValuePair<String^, int>(L"Ïåòóøèíîå ïåðî", 3));
+    core->Add(KeyValuePair<String^, int>(L"Âîëîñ êåëüïè", 5));
+}
